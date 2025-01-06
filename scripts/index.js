befe3495ae0d9d92a141dbb37fb5158f3b201e13
Sridhar Tiwari <tiwari4002@outlook.com>
2025-01-05 19:32:22 -0500

    Stage 4 Complete

--- scripts/index.js
+++ scripts/index.js
@@ -28,8 +28,19 @@ const initialCards = [
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector(".modal");
 const closeModalButton = document.querySelector(".modal__close-button");
+const profileFormElement = document.querySelector(".modal__form");
+const nameInput = document.querySelector(
+  ".modal__input[placeholder='Bessie Coleman']"
+);
+const jobInput = document.querySelector(
+  ".modal__input[placeholder='Civil Aviator']"
+);
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__info p");
 
 function openModal() {
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
   editProfileModal.classList.add("modal_opened");
 }
 
@@ -39,3 +50,32 @@ function closeModal() {
 
 editProfileButton.addEventListener("click", openModal);
 closeModalButton.addEventListener("click", closeModal);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+  closeModal();
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".gallery__cards");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+});

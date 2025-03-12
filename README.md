395bb783d22a1524eb305eeb5c272a0f41b83004
Sridhar Tiwari <tiwari4002@outlook.com>
2025-03-11 21:22:42 -0400

    Test Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project 5: Spots
+
+### Overview
+
+- Intro
+- Figma
+- Images
+
+**Intro**
+
+This project is made so all the elements are displayed correctly on popular screen sizes. We recommend investing more time in completing this project, since it's more difficult than previous ones.  
+This project is made as a mockup profile page for social purposes. Users can add new posts, like, and edit their profile. It is a responsive page for multiple device sizes.
+
+<img src="./images/mobile.png" alt="Mobile view of Spots application">
+<img src="./images/tablet.png" alt="Tablet view of Spots application">
+<img src="./images/laptop.png" alt="Desktop view of Spots application">
+
+Link: https://tsri-code.github.io/se_project_spots/
+
+Video: https://drive.google.com/file/d/1Fbw6yX7MITJukJTgeuemsldq-TlIN-LP/view?usp=sharing
+
+**Figma**
+
+- [Link to the project on Figma](https://www.figma.com/design/1qCS9RkiKiVquBhpOJqjZ0/Sprint-5-Project%3A-Spots?node-id=2201-1078&t=wBhbxezXS0PChyNg-0)
+
+**Images**
+
+The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster.
+
+Good luck and have fun!
---
+++ scripts/index.js
@@ -0,0 +1,138 @@
+// Initial cards data - array of objects containing card information
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Landscape view",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+];
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+const newPostButton = document.querySelector(".profile__add-button");
+const modals = document.querySelectorAll(".modal");
+const editProfileModal = modals[0]; // First modal is edit profile
+const newPostModal = modals[1]; // Second modal is new post
+const closeButtons = document.querySelectorAll(".modal__close-btn");
+
+const profileModal = document.querySelector("#edit-profile-modal");
+const profileForm = profileModal.querySelector("#edit-profile-form");
+const nameInput = document.querySelector("#name-input");
+const descriptionInput = document.querySelector("#description-input");
+const cardModal = document.querySelector("#new-post-modal");
+const cardForm = cardModal.querySelector("#new-post-form");
+const cardNameInput = cardModal.querySelector("#caption-input");
+const cardLinkInput = cardModal.querySelector("#image-link");
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
+const previewModal = document.querySelector("#view-photo");
+const previewModalImageEl = previewModal.querySelector(".modal__image");
+const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
+
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".gallery__cards");
+
+function openModal(modal) {
+  modal.classList.add("modal__opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal__opened");
+}
+
+function openEditProfileModal() {
+  nameInput.value = profileNameElement.textContent;
+  descriptionInput.value = profileDescriptionElement.textContent;
+  openModal(editProfileModal);
+}
+
+function openNewPostModal() {
+  openModal(newPostModal);
+}
+
+// Add event listeners
+editProfileButton.addEventListener("click", openEditProfileModal);
+newPostButton.addEventListener("click", openNewPostModal);
+
+// Close buttons for all modals
+closeButtons.forEach((button) => {
+  const modal = button.closest(".modal");
+  button.addEventListener("click", () => closeModal(modal));
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileDescriptionElement.textContent = descriptionInput.value;
+  closeModal(editProfileModal);
+}
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+  const cardData = {
+    name: cardNameInput.value,
+    link: cardLinkInput.value,
+  };
+  const cardElement = getCardElement(cardData);
+  cardList.prepend(cardElement);
+  closeModal(newPostModal);
+  cardForm.reset();
+}
+
+cardForm.addEventListener("submit", handleNewPostFormSubmit);
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-button_active");
+  });
+  cardDeleteBtn.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", () => {
+    openModal(previewModal);
+    previewModalImageEl.src = data.link;
+    previewModalCaptionEl.textContent = data.name;
+  });
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+});
